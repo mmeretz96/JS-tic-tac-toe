66fd212b648e4348a54486eb17736dbe96bd1f9e
matthias <meretzmatthias@yahoo.de>
2023-01-01 13:44:56 +0100

    Fix bugs

--- script.js
+++ script.js
@@ -1,167 +1,216 @@
-//delete after debugging
-const log = console.log
-
 const CellFactory = () => {
-  let marker = undefined
+  let marker = undefined;
 
   const setMarker = (newMarker) => {
-    marker = newMarker
-  }
+    marker = newMarker;
+  };
+
   const getMarker = () => {
-    return marker
-  }
+    return marker;
+  };
 
-  return{
+  return {
     getMarker,
-    setMarker
-  }
-}
+    setMarker,
+  };
+};
 
-const DomController = ((()=>{
-  const boardContainer = document.querySelector(".board")
+const DomController = (() => {
+  const boardContainer = document.querySelector(".board");
+
   const displayCells = () => {
-    boardContainer.innerHTML = ""
-    for(let i = 0; i<9; i++){
-      const div = document.createElement("div")
-      div.classList.add("cell")
-      div.setAttribute("data-number", i)
-      boardContainer.appendChild(div)
+    boardContainer.innerHTML = "";
+    for (let i = 0; i < 9; i++) {
+      const div = document.createElement("div");
+      div.classList.add("cell");
+      div.setAttribute("data-number", i);
+      boardContainer.appendChild(div);
     }
-  }
+  };
 
   const placeMarker = (pos, marker) => {
-    cells = document.querySelectorAll(".cell")
+    cells = document.querySelectorAll(".cell");
     cells.forEach((cell, index) => {
-      if(pos == index){
-        const img = document.createElement("img")
-        img.setAttribute("src","images/"+marker+".png")
-        img.classList.add("marker")
-        img.setAttribute("data-number", index)
-        cell.appendChild(img)
+      if (pos == index) {
+        const img = document.createElement("img");
+        img.setAttribute("src", "images/" + marker + ".png");
+        img.classList.add("marker");
+        img.setAttribute("data-number", index);
+        cell.appendChild(img);
       }
-    })
-  }
+    });
+  };
 
   const displayMessage = (message, color) => {
-    const messageBox = document.querySelector(".messageBox")
-    messageBox.innerHTML = message
-    messageBox.style.backgroundColor = color
-  }
-  
-  return{
+    const messageBox = document.querySelector(".messageBox");
+    messageBox.innerHTML = message;
+    messageBox.style.backgroundColor = color;
+  };
+
+  const highlightCells = (cells) => {
+    cells.forEach((cell) => {
+      let cellToColor = document.querySelector(`.cell[data-number= "${cell}"]`);
+      cellToColor.style.backgroundColor = "red";
+    });
+  };
+
+  return {
     displayCells,
     placeMarker,
-    displayMessage
-  }
-}))()
-
-
+    displayMessage,
+    highlightCells,
+  };
+})();
 
 const Gameboard = (() => {
-  let cells = []
+  let cells = [];
+  let cellsOccupied = 0;
   const winCombinations = [
-    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
-  ]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
   const init = () => {
-    cells = []
-    for(let i = 0; i<9; i++){
-      cells.push(CellFactory())
+    cells = [];
+    cellsOccupied = 0;
+    for (let i = 0; i < 9; i++) {
+      cells.push(CellFactory());
     }
-  }
-  
+  };
+
   const placeMarker = (pos, marker) => {
-    if(cells[pos].getMarker()) return false
-    cells[pos].setMarker(marker)
-    DomController.placeMarker(pos, marker)
-    return true
-  }
+    if (cells[pos].getMarker()) return false;
+    cells[pos].setMarker(marker);
+    cellsOccupied++;
+    return true;
+  };
 
   const getWinCombination = (pos) => {
-   winCombinations.forEach(combination => {
-    log(combination.includes(pos))
-    if(combination.includes(pos)){
-    const markers = []
-    combination.forEach(p => {
-      markers.push(getCell(p).getMarker())
-    })
-    log(markers)
+    let winCombination;
+    winCombinations.forEach((combination) => {
+      if (combination.includes(parseInt(pos))) {
+        const markers = [];
+        combination.forEach((p) => {
+          markers.push(getCell(p).getMarker());
+        });
+        if (markers[0] != undefined) {
+          if (markers[0] == markers[1] && markers[0] == markers[2]) {
+            winCombination = combination;
+          }
+        }
+      }
+    });
+    if (cellsOccupied == 9) {
+      return "draw";
     }
-   })
-  }
 
-  const getCell = (pos) => cells[pos]
+    return winCombination;
+  };
 
-  const getCells = () => cells
-  init()
-  return{
+  const getCell = (pos) => cells[pos];
+
+  const getCells = () => cells;
+  init();
+  return {
     getCells,
     getWinCombination,
     placeMarker,
     init,
-    getCell
-  }
+    getCell,
+  };
 })();
 
-
-
 const GameController = (() => {
-  let players = [{num: 0, name: "player", marker: "x"}, {num: 1, name: "computer", marker: "o"}]
-  let activePlayer = players[0]
+  let players = [
+    { num: 0, name: "Player", marker: "x" },
+    { num: 1, name: "Computer", marker: "o" },
+  ];
+  let activePlayer = players[0];
+  let hasEnded = false;
 
   const switchActivePlayer = () => {
-    activePlayer = players[(activePlayer.num+1)%2]
-    DomController.displayMessage(activePlayer.name + "'s turn!" , "green")
-  }
+    activePlayer = players[(activePlayer.num + 1) % 2];
+    DomController.displayMessage(activePlayer.name + "'s turn!", "green");
+  };
 
   const init = () => {
-    DomController.displayCells()
-    document.querySelectorAll(".cell").forEach(cell => {
-      cell.addEventListener("click", e => {
-       handleCellClick(e.target)
-      })
+    hasEnded = false;
+    DomController.displayCells();
+    document.querySelectorAll(".cell").forEach((cell) => {
+      cell.addEventListener("click", (e) => {
+        handleCellClick(e.target);
+      });
 
-      cell.addEventListener("mouseover", e => {
-        const boardCell = Gameboard.getCell(e.target.getAttribute("data-number"))
-        if(boardCell.getMarker()) return
-        e.target.style.backgroundImage = `url(images/${activePlayer.marker}.png)` 
-      })
+      cell.addEventListener("mouseover", (e) => {
+        if (hasEnded) return;
+        const boardCell = Gameboard.getCell(
+          e.target.getAttribute("data-number")
+        );
+        if (boardCell.getMarker()) return;
+        e.target.style.backgroundImage = `url(images/${activePlayer.marker}.png)`;
+      });
 
-      cell.addEventListener("mouseout", e => {
-        e.target.style.backgroundImage = ``
-      })
-    })
-  }
+      cell.addEventListener("mouseout", (e) => {
+        e.target.style.backgroundImage = ``;
+      });
+    });
+    switchActivePlayer();
+  };
 
-  const handleCellClick = (cell) =>{
-    cell.style.backgroundImage = ``
-    if (!Gameboard.placeMarker(cell.getAttribute("data-number") , activePlayer.marker)){
-      DomController.displayMessage("Cell is occupied already!", "#440044")
-      return
+  const handleCellClick = (cell) => {
+    if (hasEnded) return;
+    cell.style.backgroundImage = ``;
+    if (
+      !Gameboard.placeMarker(
+        cell.getAttribute("data-number"),
+        activePlayer.marker
+      )
+    ) {
+      DomController.displayMessage("Cell is occupied already!", "#440044");
+      return;
     }
 
-    //check for win
-    Gameboard.getWinCombination(cell.getAttribute("data-number"))
-    switchActivePlayer()
+    DomController.placeMarker(
+      cell.getAttribute("data-number"),
+      activePlayer.marker
+    );
 
-  }
+    const wincombo = Gameboard.getWinCombination(
+      cell.getAttribute("data-number")
+    );
+    if (wincombo) {
+      if (wincombo == "draw") {
+        DomController.displayMessage("DRAW!", "red");
+        end();
+        return;
+      }
+      DomController.displayMessage(activePlayer.name + " won", "red");
+      DomController.highlightCells(wincombo);
+      end();
+      return;
+    }
+    switchActivePlayer();
+  };
 
+  const end = () => {
+    hasEnded = true;
+  };
 
-
-  document.querySelector(".reset").addEventListener("click", e => {
-    Gameboard.init()
-    init()
-  })
+  document.querySelector(".reset").addEventListener("click", (e) => {
+    Gameboard.init();
+    init();
+  });
 
   return {
     switchActivePlayer,
-    init
-  }
- })()
+    init,
+  };
+})();
 
-
-
-GameController.init()
-
-
-
-
+GameController.init();
